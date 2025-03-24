441f275d69cdce873ec57360b6739dbbb81a86fd
kusztelson <kusztelson@gmail.com>
2025-03-24 15:57:28 +0100

    angular material + basic layout

--- frontend/iap/src/app/app.routes.ts
+++ frontend/iap/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CarsComponent } from './cars/cars.component';
+import { ReservationsComponent } from './reservations/reservations.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "cars", component: CarsComponent},
+    {path: "reservations", component: ReservationsComponent},
+];
