25a69bde5b64586fcde88b5dabb4607eae25cf2c
nbiernacka <natti.westie0@gmail.com>
2025-03-26 22:44:07 +0100

    Reservations - basic cards with images and cancel button

--- frontend/iap/src/app/reservations/reservations.component.ts
+++ frontend/iap/src/app/reservations/reservations.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-reservations',
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule, MatCardModule, MatButtonModule],
   templateUrl: './reservations.component.html',
   styleUrl: './reservations.component.scss'
 })
