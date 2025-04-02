8a39361b6372cfe91a1d53e5aaf277cde5b39d06
mkopanik <akopanpl@gmail.com>
2025-04-02 09:33:22 +0200

    dynamic list of cars

--- frontend/iap/src/app/cars/cars.component.ts
+++ frontend/iap/src/app/cars/cars.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-cars',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './cars.component.html',
-  styleUrl: './cars.component.scss'
+  styleUrl: './cars.component.scss',
 })
-export class CarsComponent {
-
-}
+export class CarsComponent {}
