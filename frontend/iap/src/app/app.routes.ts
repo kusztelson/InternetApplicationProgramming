2c0cf4b51f2a64b5bd37d2e31c2493911bc771ab
kusztelson <kusztelson@gmail.com>
2025-04-11 10:24:19 +0200

    Login page, interceptor, guard. No redirect after login.

--- frontend/iap/src/app/app.routes.ts
+++ frontend/iap/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { CarsComponent } from './cars/cars.component';
 import { ReservationsComponent } from './reservations/reservations.component';
 import { HelloComponent } from './hello/hello.component';
+import { LoginComponent } from './login/login.component';
+import { userGuard } from './core/user.guard';
 
 export const routes: Routes = [
-    {path: "", redirectTo: "/hello", pathMatch: "full"},
-    {path: "hello", component: HelloComponent},
+    {path: "", component: LoginComponent},
+    {path: "hello", component: HelloComponent, canActivate: [userGuard]},
     {path: "cars", component: CarsComponent},
     {path: "reservations", component: ReservationsComponent},
 ];
---
+++ frontend/iap/src/app/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { LoginService } from './login.service';
+
+@Component({
+  selector: 'app-login',
+  imports: [ ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private loginService: LoginService) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      this.loginService.login(this.loginForm.value);
+    }
+  }
+}
