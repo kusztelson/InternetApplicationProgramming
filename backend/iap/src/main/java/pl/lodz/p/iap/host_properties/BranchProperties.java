ff25428bbd50e5523aa044fbc29b9ee3fcf22f68
Vitek1625 <witold.kubisiak@gmail.com>
2025-05-30 12:48:46 +0200

    Changed profile not host to dev

--- backend/iap/src/main/java/pl/lodz/p/iap/host_properties/BranchProperties.java
+++ backend/iap/src/main/java/pl/lodz/p/iap/host_properties/BranchProperties.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("!host")
+@Profile("dev")
 @ConfigurationProperties(prefix = "contact")
 public class BranchProperties extends PropertyHandler {
 
